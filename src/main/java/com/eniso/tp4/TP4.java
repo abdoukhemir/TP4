b31ba99f534d5d5a9d9187088d129647415ff1b3
Khemir Abderrahmen <abderahmen.khemir@gmail.com>
2024-04-02 18:04:50 +0100

    TP4

---
+++ src/main/java/com/eniso/tp4/TP4.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.eniso.tp4;
+
+
+
+/**
+ *
+ * @author mezen
+ */
+public class TP4 {
+
+    public static void main(String[] args){System.out.println("hello");
+    }
+}
